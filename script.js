fc52800c954bc0b3bc52b019beed8d322173359e
LAKHAL ADNANE <adnanelakhal19@gmail.com>
2020-11-21 18:35:55 +0100

    new commit

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const quotecontainer = document.getElementById('quotecont');
 const quotes = document.getElementById('quote');
 const author = document.getElementById('author');
+const playbtn = document.getElementById('anim');
 const btn = document.getElementById('new_quote');
 const loader = document.getElementById('loader');
+const toggleswitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById('toggle-icon');
+const twitterbnt = document.getElementById('twitter');
+const draws = document.getElementById('canv');
+
 
 // loding
 function loading(){
@@ -27,20 +33,49 @@ async function callquote(){
         console.log(data.quoteAuthor);
         author.innerText = data.quoteAuthor;
         quotes.innerText = data.quoteText;
-        
     }catch(error){
         console.log(error);
 
     }
-    showload();
-    
+    showload()    
 }
 
 
+function tweetequote(){
+    const quote = quotes.innerText;
+    const Author = author.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+
+}
+twitterbnt.addEventListener('click',tweetequote);
 btn.addEventListener('click', callquote);
 
 
 callquote();
 
+// Dark Mode Styles
+function darkMode() {
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+}
+// Light Mode Styles
+function lightMode() {
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+}
+// switch to other mode
+function switchmode(event){
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    darkMode();
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    lightMode();
+  }
+}
+
+
+toggleswitch.addEventListener('change',switchmode);
 
 
